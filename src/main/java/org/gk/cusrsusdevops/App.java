c12812cd5fbadad188190ece8122258a75f014e2
Horou <nicolas.nemouthe@gmail.com>
2021-09-09 13:26:18 +0000

    first commit

---
+++ src/main/java/org/gk/cusrsusdevops/App.java
@@ -0,0 +1,13 @@
+package org.gk.cusrsusdevops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
